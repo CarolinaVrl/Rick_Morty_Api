7497e000ac79646a1df447fb55226ad82c1c3187
Diana Villarreal <dianacarolinav97@gmail.com>
2023-06-06 17:22:10 -0500

    feat: get info api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 import "./App.css";
-
-// const [location, setLocation] = useState("")
-const id = Math.round(Math.random()*126)
+import axios from "axios";
 
 function App() {
-  return <div className="App">
-    <h1></h1>
-    
- 
-  </div>;
+  const id = Math.round(Math.random() * 126);
+  const [location, setLocation] = useState([]);
+  useEffect(()=>{
+    axios.get(`https://rickandmortyapi.com/api/location/${id}`)
+    .then(res => setLocation(res.data))
+  },[])
+  console.log(location);
+  return (
+    <div className="App">
+      <h1>{id}</h1>
+      
+    </div>
+  );
 }
 
 export default App;
