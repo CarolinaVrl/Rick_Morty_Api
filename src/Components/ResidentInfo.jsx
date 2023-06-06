b9aeb373484ffa1bc25563a79453148dc35c36f3
Diana Villarreal <dianacarolinav97@gmail.com>
2023-06-06 17:53:44 -0500

    feat: api resident

--- src/Components/ResidentInfo.jsx
+++ src/Components/ResidentInfo.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const ResidentInfo = ({url}) => {
-    return (
-        <div>
-            soy un residente
-            
-        </div>
-    );
+const ResidentInfo = ({ url }) => {
+  const [resident, setResident] = useState({});
+  useEffect(() => {
+    axios.get(`${url}`)
+    .then((res) => setResident(res.data));
+  }, []);
+
+  
+  return <div>
+    <img src="" alt="" />
+    <section></section>
+  </div>;
 };
 
 export default ResidentInfo;
