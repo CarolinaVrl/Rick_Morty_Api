c5bf20d46f803247f9c2a62a2625b3a503c65240
Diana Villarreal <dianacarolinav97@gmail.com>
2023-06-06 16:41:00 -0500

    feat: random id

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 
 import "./App.css";
 
+// const [location, setLocation] = useState("")
+const id = Math.round(Math.random()*126)
 
 function App() {
   return <div className="App">
+    <h1></h1>
+    
  
   </div>;
 }
