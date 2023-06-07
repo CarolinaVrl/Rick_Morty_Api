100068c5f8ddecd39c2fe08ad2a78b33d743cb73
Diana Villarreal <dianacarolinav97@gmail.com>
2023-06-07 09:10:13 -0500

    feat: residetn info api

--- src/Components/ResidentInfo.jsx
+++ src/Components/ResidentInfo.jsx
@@ -4,15 +4,32 @@ import React, { useEffect, useState } from "react";
 const ResidentInfo = ({ url }) => {
   const [resident, setResident] = useState({});
   useEffect(() => {
-    axios.get(`${url}`)
-    .then((res) => setResident(res.data));
+    axios.get(`${url}`).then((res) => setResident(res.data));
   }, []);
 
+  return (
+    <div>
+      <img src={resident.image} alt="" />
+      <hr />
+      <div>
+        <p>Raza</p>
+        <p>{resident.species}</p>
+      </div>
+      <div>
+        <p>Origen</p>
+        <p>{resident.origin?.name}</p>
+      </div>
+      <div>
+        <p>Aparición en episodios</p>
+        <p>{resident.episode?.length}</p>
+      </div>
   
-  return <div>
-    <img src="" alt="" />
-    <section></section>
-  </div>;
+
+      <div>
+        <p>{resident.status}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ResidentInfo;
